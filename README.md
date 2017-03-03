8675eada1c8a676ecc27a1cd4e129f045438742b
Yutian Li <hotpxless@gmail.com>
2017-03-02 17:51:59 -0800

    [master] Init.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# todoist-tui
-Text user interface for Todoist
+# Todoist TUI
+
+Text user interface for Todoist.
+
+The native Todoist client is already good enough. But I wanted something to run in terminal for greater efficiency. I created this project also to try out some Haskell libraries.
+
+## Usage
+
+Put your Todoist API token under `~/.config/todoist`.
+
+Current supported functionalities include:
+* Add item.
+* Complete item.
+
+I will try to support due dates and modifying items if I somehow need to use them. Currently everything goes into Inbox project and stays there. This is my workflow. So I am not going to support multiple projects.
+
+## Keys
+
+* <kbd>j</kbd> Move selection down.
+* <kbd>k</kbd> Move selection up.
+* <kbd>c</kbd> Start creating new item.
+* <kbd>q</kbd> Commit and quit.
+* <kbd>Enter</kbd> Finish creating new item.
+* <kbd>Space</kbd> Mark as complete.
+
---
+++ src/Main.hs
@@ -0,0 +1,340 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+module Main where
+
+import Data.List
+import Data.Maybe
+import System.IO
+
+import Brick.Widgets.Core ((<+>))
+import Data.Aeson ((.:))
+import Control.Lens ((&), (.~), (^.))
+
+import qualified Brick as B
+import qualified Brick.Main as BM
+import qualified Brick.Types as BT
+import qualified Brick.Util as BU
+import qualified Brick.Widgets.Core as BWC
+import qualified Brick.Widgets.Center as BWCenter
+import qualified Brick.Widgets.List as BWL
+import qualified Brick.Widgets.Edit as BWE
+import qualified Brick.Widgets.Border as BWB
+import qualified Brick.AttrMap as BA
+import qualified Graphics.Vty as GV
+import qualified Data.Vector as DV
+import qualified Brick.Focus as BF
+import qualified Data.Text.Zipper as DTZ
+import qualified Network.Wreq as NW
+import qualified Data.Text as DT
+import qualified Data.UUID.V4 as DUV4
+import qualified Data.UUID as DU
+import qualified Data.ByteString.Lazy as DBL
+import qualified Data.Aeson as DA
+import qualified System.Directory as SD
+import qualified System.FilePath.Posix as SFP
+
+data TodoistState = TodoistState
+  { items :: ItemList
+  , currentIndex :: Int
+  , focusRing :: BF.FocusRing Name
+  , editor :: BWE.Editor String Name
+  }
+
+data Name
+  = ListView
+  | Editor
+  deriving (Ord, Eq, Show)
+
+listDrawElement :: Bool -> String -> B.Widget Name
+listDrawElement _ = BWC.padRight BT.Max . BWC.str
+
+drawUI :: TodoistState -> [BT.Widget Name]
+drawUI state = [ui]
+  where
+    TodoistState { items = items
+                 , currentIndex = currentIndex
+                 , focusRing = focusRing
+                 , editor = editor
+                 } = state
+    ed = BF.withFocusRing focusRing BWE.renderEditor editor
+    l =
+      let ItemList i = items
+      in BWL.list
+           ListView
+           (DV.fromList
+              (map
+                 (\Item {content = content, due = due, itemState = itemState} ->
+                    (case itemState of
+                       New -> "[+] "
+                       Cancel -> "[X] "
+                       Delete _ -> "[X] "
+                       Existing _ -> "[ ] ") ++
+                    content)
+                 i))
+           1 &
+         BWL.listMoveTo currentIndex
+    ui =
+      BWC.vBox
+        [ BWC.str "Inbox" & BWB.borderWithLabel $
+          BWL.renderList listDrawElement True l
+        , BWC.str "> " <+> BWC.vLimit 1 ed
+        ]
+
+appEvent :: TodoistState
+         -> BT.BrickEvent Name e
+         -> BT.EventM Name (BT.Next TodoistState)
+appEvent l (BT.VtyEvent e) =
+  let TodoistState { items = items
+                   , currentIndex = currentIndex
+                   , focusRing = focusRing
+                   , editor = editor
+                   } = l
+  in case BF.focusGetCurrent focusRing of
+       Nothing -> BM.continue l
+       Just ListView ->
+         case e of
+           GV.EvKey GV.KEsc [] -> BM.halt l
+           GV.EvKey (GV.KChar ' ') [] ->
+             BM.continue
+               TodoistState
+               { items = markItem currentIndex items
+               , currentIndex = currentIndex
+               , focusRing = focusRing
+               , editor = editor
+               }
+           GV.EvKey (GV.KChar 'q') [] -> BM.halt l
+           GV.EvKey (GV.KChar 'k') [] ->
+             let newState =
+                   TodoistState
+                   { items = items
+                   , currentIndex =
+                       if 0 < currentIndex
+                         then currentIndex - 1
+                         else currentIndex
+                   , focusRing = focusRing
+                   , editor = editor
+                   }
+             in BM.continue newState
+           GV.EvKey (GV.KChar 'j') [] ->
+             let TodoistState { items = ItemList items
+                              , currentIndex = currentIndex
+                              } = l
+             in let newState =
+                      TodoistState
+                      { items = ItemList items
+                      , currentIndex =
+                          if currentIndex < (length items) - 1
+                            then currentIndex + 1
+                            else currentIndex
+                      , focusRing = focusRing
+                      , editor = editor
+                      }
+                in BM.continue newState
+           GV.EvKey (GV.KChar 'c') [] ->
+             TodoistState
+             { items = items
+             , currentIndex = currentIndex
+             , focusRing = BF.focusNext focusRing
+             , editor = editor
+             } &
+             BM.continue
+           _ -> BM.continue l
+       Just Editor ->
+         case e of
+           GV.EvKey GV.KEnter [] ->
+             let newContent = BWE.getEditContents editor & intercalate " "
+             in TodoistState
+                { items =
+                    if newContent /= ""
+                      then addItem newContent items
+                      else items
+                , currentIndex = currentIndex
+                , focusRing = BF.focusNext focusRing
+                , editor = BWE.applyEdit DTZ.clearZipper editor
+                } &
+                BM.continue
+           ev -> do
+             nextEditor <- BWE.handleEditorEvent ev editor
+             BM.continue $
+               TodoistState
+               { items = items
+               , currentIndex = currentIndex
+               , focusRing = focusRing
+               , editor = nextEditor
+               }
+appEvent l _ = BM.continue l
+
+data ResourceType = Projects | Items
+
+resourceTypeToString :: ResourceType -> String
+resourceTypeToString Projects = "projects"
+resourceTypeToString Items = "items"
+
+getResource :: String -> ResourceType -> IO DBL.ByteString
+getResource token resourceType = do
+  let url = "https://todoist.com/API/v7/sync"
+  let opts =
+        NW.defaults & NW.param "sync_token" .~ ["*"] & NW.param "resource_types" .~
+        [DT.pack $ "[\"" ++ resourceTypeToString resourceType ++ "\"]"] &
+        NW.param "token" .~
+        [DT.pack token]
+  r <- NW.getWith opts url
+  return $ r ^. NW.responseBody
+
+data ProjectList =
+  ProjectList [Project] deriving (Show)
+
+data Project = Project
+  { name :: String
+  , inbox :: Bool
+  , id :: Int
+  } deriving (Show)
+
+instance DA.FromJSON Project where
+  parseJSON =
+    DA.withObject "Project" $ \o ->
+      Project <$> o .: "name" <*> o .: "inbox_project" <*> o .: "id"
+
+instance DA.FromJSON ProjectList where
+  parseJSON =
+    DA.withObject "ProjectList" $ \o ->
+      (o .: "projects") >>= ((fmap ProjectList) . DA.parseJSON)
+
+data ItemList =
+  ItemList [Item]
+  deriving (Show)
+
+data ItemState
+  = New
+  | Cancel
+  | Delete Int
+  | Existing Int
+  deriving (Show)
+
+data Item = Item
+  { content :: String
+  , due :: String
+  , itemState :: ItemState
+  } deriving (Show)
+
+instance DA.FromJSON ItemList where
+  parseJSON =
+    DA.withObject "ItemList" $ \o ->
+      (o .: "items") >>= fmap ItemList . DA.parseJSON
+
+instance DA.FromJSON Item where
+  parseJSON =
+    DA.withObject "Item" $ \o ->
+      Item <$> o .: "content" <*> o .: "date_string" <*>
+      (o .: "id" >>= return . Existing)
+
+addItem :: String -> ItemList -> ItemList
+addItem content (ItemList l) = l ++ [Item content "" New] & ItemList
+
+markItem :: Int -> ItemList -> ItemList
+markItem 0 (ItemList (head:tail)) =
+  let Item {content = content, due = due, itemState = itemState} = head
+  in Item
+     { content = content
+     , due = due
+     , itemState =
+         case itemState of
+           Delete i -> Existing i
+           Existing i -> Delete i
+           New -> Cancel
+           Cancel -> New
+     } :
+     tail & ItemList
+markItem idx (ItemList (head:tail)) =
+  let ItemList newTail = markItem (idx - 1) (ItemList tail)
+  in head : newTail & ItemList
+markItem _ (ItemList []) = ItemList []
+
+
+getProjects :: String -> IO ProjectList
+getProjects token = do
+  res <- getResource token Projects
+  let decoded = DA.decode res :: Maybe ProjectList
+  case decoded of
+    Nothing -> return $ ProjectList []
+    Just a -> return $ a
+
+getItems :: String -> IO ItemList
+getItems token = do
+  res <- getResource token Items
+  let decoded = DA.decode res :: Maybe ItemList
+  case decoded of
+    Nothing -> return $ ItemList []
+    Just a -> return $ a
+
+commitItems :: String -> ItemList -> IO ()
+commitItems token (ItemList items) = do
+  let url = "https://todoist.com/API/v7/sync"
+  strList <-
+    foldl
+      (\prev Item {content = content, due = due, itemState = itemState} -> do
+         prevStrList <- prev
+         case itemState of
+           New -> do
+             uuid <- DUV4.nextRandom
+             let newCommand =
+                   "{\"type\":\"item_add\",\"temp_id\":\"\",\"uuid\":\"" ++
+                   DU.toString uuid ++
+                   "\",\"args\":{\"content\":\"" ++
+                   content ++
+                   "\"}}"
+             return $ newCommand : prevStrList
+           Delete id -> do
+             uuid <- DUV4.nextRandom
+             let newCommand =
+                   "{\"type\":\"item_close\",\"temp_id\":\"\",\"uuid\":\"" ++
+                   DU.toString uuid ++
+                   "\",\"args\":{\"id\":" ++
+                   show id ++
+                   "}}"
+             return $ newCommand : prevStrList
+           _ -> return prevStrList)
+      (return [])
+      items
+  if length strList == 0
+    then return ()
+    else do
+      let commands = "[" ++ intercalate "," strList ++ "]"
+      let opts =
+            NW.defaults & NW.param "token" .~ [DT.pack token] &
+            NW.param "commands" .~
+            [DT.pack commands]
+      res <- NW.getWith opts url
+      if res ^. NW.responseStatus . NW.statusCode /= 200
+        then putStrLn "An error occured."
+        else return ()
+
+main :: IO ()
+main = do
+  home <- SD.getHomeDirectory
+  withFile (SFP.combine home ".config/todoist") ReadMode $ \handle -> do
+    token <- hGetLine handle
+    itemList <- getItems token
+    let state =
+          TodoistState
+          { items = itemList
+          , currentIndex = 0
+          , focusRing = BF.focusRing [ListView, Editor]
+          , editor = BWE.editor Editor (BWC.str . unlines) Nothing ""
+          }
+    let app =
+          BM.App
+          { appDraw = drawUI
+          , appChooseCursor = BM.showFirstCursor
+          , appHandleEvent = appEvent
+          , appStartEvent = return
+          , appAttrMap =
+              [(BWL.listSelectedAttr, BU.on GV.black GV.white)] &
+              BA.attrMap GV.defAttr &
+              const
+          }
+    finalState <- BM.defaultMain app state
+    let TodoistState {items = items} = finalState
+    commitItems token items
+    return ()
